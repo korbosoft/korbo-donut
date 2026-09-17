2cb038bb06f63b9b0695d41ddb5b32127a229a26
Korbo <korbin.m.marshall@gmail.com>
2026-09-16 20:55:01 -0500

    most important change in the entire history of KDS

--- main/text.h
+++ main/text.h
@@ -6,5 +6,5 @@
 extern int format_info(const char *prefix, const char *input, char output[83], bool pad);
 extern void format_splash(const char *input, char *output);
 extern int print(const char *str);
-extern char* u82Str(char* buf, u8 n);
+extern char *u82Str(char *buf, u8 n);
 #endif
